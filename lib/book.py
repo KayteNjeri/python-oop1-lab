74684521e9dc56c3f218e23d04ca5c4355fa639d
KayteNjeri <canjeri91@gmail.com>
2026-07-18 07:26:44 +0300

    Create a Book Class with page_count validation and turn_page

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def __init__(self, title, page_count):
+        self.title = input("Enter the title of the book: ")
         
+        self.page_count = input("Enter the page count: ")
+        try:
+            self.page_count = int(self.page_count)
+        except ValueError:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print(f"Flipping the page...wow, you read fast!")
+
+book = Book()
+book.turn_page()
