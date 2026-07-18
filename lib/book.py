4714c124df0ff215f493df505948fdb4ca4537a3
KayteNjeri <canjeri91@gmail.com>
2026-07-18 08:41:20 +0300

    Fixed Book and Coffee classes to pass the pytest

--- lib/book.py
+++ lib/book.py
@@ -2,16 +2,21 @@
 
 class Book:
     def __init__(self, title, page_count):
-        self.title = input("Enter the title of the book: ")
-        
-        self.page_count = input("Enter the page count: ")
-        try:
-            self.page_count = int(self.page_count)
-        except ValueError:
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
             print("page_count must be an integer")
 
     def turn_page(self):
-        print(f"Flipping the page...wow, you read fast!")
+        print("Flipping the page...wow, you read fast!")
 
-book = Book()
-book.turn_page()
+
--- lib/coffee.py
+++ lib/coffee.py
@@ -2,16 +2,19 @@
 
 class Coffee:
     def __init__(self, size, price):
-        self.size = input("Enter the coffee size (Small, Medium, or Large): ")
-        if self.size not in ["Small", "Medium", "Large"]:
-            print("size must be Small, Medium, or Large.")
-            self.size = input("Enter the coffee size (Small, Medium, or Large): ")
-        
-        self.price = float(input("Enter the price: "))
-
+        self.size = size
+        self.price = price
+    @property
+    def size(self):
+        return self._size
+    @size.setter
+    def size(self, value):
+        if value in ["Small", "Medium", "Large"]:
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+            
     def tip(self):
-        print(f"This coffee is great, here's a tip!")
+        print("This coffee is great, here\u2019s a tip!")
         self.price += 1
 
-coffee = Coffee()
-coffee.tip()
