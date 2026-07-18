c5b50f43def0b7c7805c506d3850334271fb468d
KayteNjeri <canjeri91@gmail.com>
2026-07-18 07:26:44 +0300

    Create Coffee class with tip

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = input("Enter the coffee size (Small, Medium, or Large): ")
+        if self.size not in ["Small", "Medium", "Large"]:
+            print("size must be Small, Medium, or Large.")
+            self.size = input("Enter the coffee size (Small, Medium, or Large): ")
+        
+        self.price = float(input("Enter the price: "))
+
+    def tip(self):
+        print(f"This coffee is great, here's a tip!")
+        self.price += 1
+
+coffee = Coffee()
+coffee.tip()
